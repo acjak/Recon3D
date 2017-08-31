8f3100b4bb92c64e69597ac92e1a08e4ac0b626c
albusdemens <mannaro85@gmail.com>
2017-08-31 15:19:43 +0200

    Introduced (and commented) option to plot images before and after cleaning

--- getdata.py
+++ getdata.py
@@ -234,30 +234,27 @@ class makematrix():
 						IM_reb = IM.reshape(sh).mean(-1).mean(1)
 						# Calculate the expected background distribution, assuming it to
 						# be linear
-						IM_reb_2 = np.zeros([bigarray_clean_2.shape[3]/int(sz_fr[0]), bigarray_clean_2.shape[4]/int(sz_fr[0])])
-						IM_reb_3 = np.zeros([bigarray_clean_2.shape[3], bigarray_clean_2.shape[4]])
-						IM_reb_2[0,:] = IM_reb[0,:]
-						IM_reb_2[IM_reb.shape[0]-1,:] = IM_reb[IM_reb.shape[0]-1,:]
-						IM_reb_2[:,0] = IM_reb[:,0]
-						IM_reb_2[:,IM_reb.shape[0]-1] = IM_reb[:,IM_reb.shape[0]-1]
+						IM_reb_2 = np.zeros([IM.shape[0], IM.shape[1]])
 						for jj in range(1,IM_reb.shape[0]-1):
 							for kk in range(1,IM_reb.shape[1]-1):
 								I_min_x = min(IM_reb[jj,0], IM_reb[jj,IM_reb.shape[1]-1])
 								I_max_x = max(IM_reb[jj,0], IM_reb[jj,IM_reb.shape[1]-1])
 								#I_min_y = min(IM_reb[0,kk], IM_reb[IM_reb.shape[0]-1, kk])
 								#I_max_y = max(IM_reb[0,kk], IM_reb[IM_reb.shape[0]-1, kk])
-								I_eval_x = I_min_x + ((I_max_x - I_min_x) / (IM.shape[0] - 2*int(sz_fr[0]))) * (jj - int(sz_fr[0]))
-								#I_eval_y = I_min_y + ((I_max_y - I_min_y) / (IM.shape[1] - 2*int(sz_fr[0]))) * (kk - int(sz_fr[0]))
-								# For the dataset 1, we notice that the crucial component to
-								# take into account is how the background varies along Y
-								IM_reb_2[jj,kk] = I_eval_x
-								# Extend the binned image to the original size (pre-binning)
-						for jj in range(IM_reb.shape[0]):
-							for kk in range(IM_reb.shape[1]):
-								IM_reb_3[jj*int(sz_fr[0]):(jj+1)*int(sz_fr[0]), kk*int(sz_fr[0]):(kk+1)*int(sz_fr[0])] = IM_reb_2[jj,kk]
+								for uu in range(jj*int(sz_fr[0]), (jj + 1)*int(sz_fr[0])):
+									for vv in range(kk*int(sz_fr[0]), (kk + 1)*int(sz_fr[0])):
+										I_eval_x = I_min_x + ((I_max_x - I_min_x) / (IM.shape[0] - 2*int(sz_fr[0]))) * (uu - int(sz_fr[0]))
+										#I_eval_y = I_min_y + ((I_max_y - I_min_y) / (IM.shape[1] - 2*int(sz_fr[0]))) * (kk - int(sz_fr[0]))
+										# For the dataset 1, we notice that the crucial component to
+										# take into account is how the background varies along Y
+										IM_reb_2[uu,vv] = I_eval_x
 
 						IM_clean = np.zeros([IM.shape[0], IM.shape[1]])
-						IM_clean = IM - IM_reb_3
+						IM_clean = IM - IM_reb_2
+						IM_clean[0:int(sz_fr[0]),:] = 0
+						IM_clean[IM.shape[0]-int(sz_fr[0]):IM.shape[0],:] = 0
+						IM_clean[:,0:int(sz_fr[0])] = 0
+						IM_clean[:,IM.shape[0]-int(sz_fr[0]):IM.shape[0]] = 0
 						IM_clean[IM_clean < 0] = 0
 
 						# Recognize the diffraction signal and set all the
@@ -281,6 +278,16 @@ class makematrix():
 								Mask[label_image == id] = 1
 
 						IM_clean_masked = IM_clean * Mask
+
+						# Plot images before and after cleaning procedure
+						#fig = plt.figure()
+						#ax = plt.subplot(1, 2, 1)
+						#plt.imshow(IM)
+						#ax.set_title("Raw image")
+						#ax = plt.subplot(1, 2, 2)
+						#plt.imshow(IM_clean_masked)
+						#ax.set_title("Cleaned image")
+						#plt.show()
 
 						bigarray_clean_3[aa,bb,ii,:,:] = IM_clean_masked[:,:]
 
