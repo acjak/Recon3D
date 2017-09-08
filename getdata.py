fd376141a1c6b0f6fb2de5446b63369b1b98761a
albusdemens <mannaro85@gmail.com>
2017-09-08 18:24:59 +0200

    Definitive code version

--- getdata.py
+++ getdata.py
@@ -99,23 +99,17 @@ class makematrix():
 		om = self.omega[0]
 		ind = np.where(self.meta[:, 2] == om)
 		a = self.meta[ind, 0][0]
-		b = self.meta[ind, 1][0]
 
-		#gamma1 = (a - data.alpha0) / np.cos(np.radians(om))
-		gamma2 = (b - data.beta0) / np.cos(np.radians(om))
-		#self.gamma = np.sort(list(set(gamma1)))
-		self.gamma = np.sort(list(set(gamma2)))
+		gamma1 = (a - data.alpha0) / np.cos(np.radians(om))
+		self.gamma = np.sort(list(set(gamma1)))
 		self.gammaindex = np.zeros((len(self.index_list)))
 
 		for ind in self.index_list:
 			om = self.meta[ind, 2]
-			#a = self.meta[ind, 0] - data.alpha0
-			b = self.meta[ind, 1] - data.beta0
-			#gamma1 = a / np.cos(np.radians(om))
-			gamma2 = b / np.cos(np.radians(om))
+			a = self.meta[ind, 0] - data.alpha0
+			gamma1 = a / np.cos(np.radians(om))
 
-			#gammapos = np.where(self.gamma == min(self.gamma, key=lambda x: abs(x-gamma1)))[0][0]
-			gammapos = np.where(self.gamma == min(self.gamma, key=lambda x: abs(x-gamma2)))[0][0]
+			gammapos = np.where(self.gamma == min(self.gamma, key=lambda x: abs(x-gamma1)))[0][0]
 			self.gammaindex[ind] = self.gamma[gammapos]
 
 	def calcMu(self, data, ang_step, n_ang_steps):
@@ -140,6 +134,7 @@ class makematrix():
 			imgarray = data.makeImgArray(self.index_list, 50, 'linetrace')
 
 		if self.rank == 0:
+			# lena = len(self.mu)
 			lena = len(self.mufake)
 			lenb = len(self.gamma)
 			leno = len(self.omega)
@@ -152,6 +147,7 @@ class makematrix():
 				b = np.where(self.gamma == self.gammaindex[ind])  # roll
 				c = np.where(self.omega == self.meta[ind, 2])  # omega
 				# d = np.where(self.mu == met[ind, 4])
+				# print a, b, c
 				if a == [0] and b == [1] and c == [10]:
 					print ind, self.data_files[ind]
 
@@ -184,7 +180,6 @@ class makematrix():
 				# Remove zeros from I_int
 				I_int = I_int[I_int != 0]
 
-				# To calculate the background image, we need two frames at least
 				if I_int.shape[0] < 2:
 					IM_min_avg[:,:,k] = np.zeros([int(imsiz[1]), int(imsiz[0])])
 				else:
@@ -220,113 +215,26 @@ class makematrix():
 					for jj in range(bigarray.shape[4]):
 						sum_img[ii,jj] = np.sum(bigarray_clean[:,:,k,ii,jj])
 				mean_proj[k,1] = np.mean(sum_img)
-<<<<<<< HEAD
-			# Calculate the mean of the mean only considering the positive values
-			mean_mean = np.sum(mean_proj[:,1])/(np.sum(mean_proj[:,1] != 0))
-=======
 
 			mean_mean = np.mean(mean_proj[:,1])
->>>>>>> e8c03899938d1bc6260e242aaf183747f390a221
 
 			# Normalize by the mean
 			for k in range(leno):
-				if mean_proj[k,1] > 0:
-				    bigarray_clean_2[:,:,k,:,:] = bigarray_clean[:,:,k,:,:] / mean_proj[k,1] * mean_mean
+				bigarray_clean_2[:,:,k,:,:] = bigarray_clean[:,:,k,:,:] / mean_proj[k,1] * mean_mean
 			print "Raw data cleaned."
 
 			for k in range(leno):
-<<<<<<< HEAD
-				if mean_proj[k,1] > 0:
-				    mean_proj_2[k,0] = k
-				    sum_img = np.zeros([bigarray.shape[3], bigarray.shape[4]])
-				    for ii in range(bigarray.shape[3]):
-					    for jj in range(bigarray.shape[4]):
-						    sum_img[ii,jj] = np.sum(bigarray_clean_2[:,:,k,ii,jj])
-				    mean_proj_2[k,1] = np.mean(sum_img)
-			# Calculate the mean only considering nonzero values
-			mean_mean_2 = np.sum(mean_proj_2[:,1])/(np.sum(mean_proj_2[:,1] != 0))
-=======
 				mean_proj_2[k,0] = k
 				sum_img = np.zeros([bigarray.shape[3], bigarray.shape[4]])
 				for ii in range(bigarray.shape[3]):
 					for jj in range(bigarray.shape[4]):
 						sum_img[ii,jj] = np.sum(bigarray_clean_2[:,:,k,ii,jj])
 						mean_proj_2[k,1] = np.mean(sum_img)
->>>>>>> e8c03899938d1bc6260e242aaf183747f390a221
 
 			bigarray_clean_3 = np.zeros((lena, lenb, leno, int(imsiz[1]), int(imsiz[0])), dtype=np.uint16)
 			# Subtract the image background, calculated usign a frame, where we
 			# expect no diffraction signal
 			for ii in range(bigarray_clean_2.shape[2]):
-<<<<<<< HEAD
-				if mean_proj[ii,1] > 0:
-					print ii
-					for aa in range(bigarray_clean_2.shape[0]):
-						for bb in range(bigarray_clean_2.shape[1]):
-							IM = np.zeros([bigarray_clean_2.shape[3], bigarray_clean_2.shape[4]])
-							IM_raw = np.zeros([bigarray_clean_2.shape[3], bigarray_clean_2.shape[4]])
-							IM[:,:] = bigarray_clean_2[aa,bb,ii,:,:]
-							if int(sz_fr[0]) > 0:
-								# Rebin the considered plot
-								IM_reb = np.zeros([bigarray_clean_2.shape[3]/int(sz_fr[0]), bigarray_clean_2.shape[4]/int(sz_fr[0])])
-								sh = IM_reb.shape[0],IM.shape[0]//IM_reb.shape[0],IM_reb.shape[1],IM.shape[1]//IM_reb.shape[1]
-								IM_reb = IM.reshape(sh).mean(-1).mean(1)
-								# Calculate the expected background distribution, assuming it to
-								# be linear
-								IM_reb_2 = np.zeros([bigarray_clean_2.shape[3]/int(sz_fr[0]),  bigarray_clean_2.shape[4]/int(sz_fr[0])])
-								IM_reb_3 = np.zeros([bigarray_clean_2.shape[3], bigarray_clean_2.shape[4]])
-								IM_reb_2[0,:] = IM_reb[0,:]
-								IM_reb_2[IM_reb.shape[0]-1,:] = IM_reb[IM_reb.shape[0]-1,:]
-								IM_reb_2[:,0] = IM_reb[:,0]
-								IM_reb_2[:,IM_reb.shape[0]-1] = IM_reb[:,IM_reb.shape[0]-1]
-								for jj in range(1,IM_reb.shape[0]-1):
-									for kk in range(1,IM_reb.shape[1]-1):
-										I_min_x = min(IM_reb[jj,0], IM_reb[jj,IM_reb.shape[1]-1])
-										I_max_x = max(IM_reb[jj,0], IM_reb[jj,IM_reb.shape[1]-1])
-								                #I_min_y = min(IM_reb[0,kk], IM_reb[IM_reb.shape[0]-1, kk])
-								                #I_max_y = max(IM_reb[0,kk], IM_reb[IM_reb.shape[0]-1, kk])
-									        I_eval_x = I_min_x + ((I_max_x - I_min_x) / (IM.shape[0] - 2*int(sz_fr[0]))) * (jj -     int(sz_fr[0]))
-								                #I_eval_y = I_min_y + ((I_max_y - I_min_y) / (IM.shape[1] - 2*int(sz_fr[0]))) * (kk - int(sz_fr[0]))
-								                # For the dataset 1, we notice that the crucial component to
-								                # take into account is how the background varies along Y
-								                IM_reb_2[jj,kk] = I_eval_x
-								                # Extend the binned image to the original size (pre-binning)
-								for jj in range(IM_reb.shape[0]):
-									for kk in range(IM_reb.shape[1]):
-										IM_reb_3[jj*int(sz_fr[0]):(jj+1)*int(sz_fr[0]), kk*int(sz_fr[0]):(kk+1)*int(sz_fr[0])] = IM_reb_2[jj,kk]
-
-								IM_clean = np.zeros([IM.shape[0], IM.shape[1]])
-							        IM_clean = IM - IM_reb_3
-							        IM_clean[IM_clean < 0] = 0
-								
-								# Recognize the diffraction signal and set all the
-	 						        # outside pixels to zero. We do so by making a mask
-	 						        IM_clean_bin = np.zeros([IM.shape[0], IM.shape[1]])
-	 						        IM_clean_bin[IM_clean > int(bin_thr[0])] = 1
-
-	 						        Cleared = ndimage.binary_fill_holes(IM_clean_bin).astype(int)
-	 						        Dilated = erosion(dilation(Cleared, disk(1)), disk(1))
-	 						        Dilated_c = ndimage.binary_fill_holes(Dilated).astype(int)
-
-	 						        # Label image regions
-	 						        label_image = label(Dilated_c)
-
-	 							Mask = np.zeros([IM_clean.shape[0], IM_clean.shape[1]])
-	 							IM_clean_masked = np.zeros([IM_clean.shape[0], IM_clean.shape[1]])
-	 							for region in regionprops(label_image):
-	                                                                 #Take regions with large enough areas
-	 								if region.area >= 100:
-	 									id = region.label
-	 									Mask[label_image == id] = 1
-
-	 							IM_clean_masked = IM_clean * Mask
-
-						        else:
-							        IM_clean_masked = np.zeros([IM.shape[0], IM.shape[1]])
-							        IM_clean_masked[IM < bin_thr[0])] = 0
-
-							bigarray_clean_3[aa,bb,ii,:,:] = IM_clean_masked[:,:]
-=======
 				print ii
 				for aa in range(bigarray_clean_2.shape[0]):
 					for bb in range(bigarray_clean_2.shape[1]):
@@ -385,7 +293,6 @@ class makematrix():
 						IM_clean_masked = IM_clean * Mask
 
 						bigarray_clean_3[aa,bb,ii,:,:] = IM_clean_masked[:,:]
->>>>>>> e8c03899938d1bc6260e242aaf183747f390a221
 
 			print "Morphology operations performed."
 
